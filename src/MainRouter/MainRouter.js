a067b29b86cc95d446415d0ca6892a87f5cc5cd2
Gregory.Johnson <3531op@gmail.com>
2021-08-07 10:33:19 -0400

    starting to lay out app components

---
+++ src/MainRouter/MainRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from '../components/Login/Login';
+import SignUp from '../components/SignUp/SignUp';
+import UsersRecipes from '../components/UsersRecipes/UsersRecipes';
+
+export default function MainRouter() {
+	return (
+		<div>
+			<Router>
+				<Switch>
+					<Route>
+						<Login />
+					</Route>
+					<Route>
+						<SignUp />
+					</Route>
+					<Route>
+						<UsersRecipes />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
+}
