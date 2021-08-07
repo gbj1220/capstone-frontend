41ac40871c5d63859d7070f9e9ae3c530379b56e
Gregory.Johnson <3531op@gmail.com>
2021-08-07 12:11:42 -0400

    created a signin form

--- src/MainRouter/MainRouter.js
+++ src/MainRouter/MainRouter.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from '../components/Login/Login';
-import SignUp from '../components/SignUp/SignUp';
+import SignUp from '../components/Login/Login';
 import UsersRecipes from '../components/UsersRecipes/UsersRecipes';
 
 export default function MainRouter() {
