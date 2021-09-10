579d9cdd8de6a8bfa8148c3114298fed860f521d
Gregory.Johnson <3531op@gmail.com>
2021-09-10 15:55:45 -0400

    Changed height of footer in footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,31 +3,31 @@ import { makeStyles } from '@material-ui/core';
 import { AppBar, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-	appBar: {
-		top: 'auto',
-		bottom: 0,
-		height: 100,
-		backgroundColor: '#505050',
-	},
+    appBar: {
+        top: 'auto',
+        bottom: 0,
+        height: 50,
+        backgroundColor: '#505050',
+    },
 }));
 
 export default function Footer() {
-	const classes = useStyles();
-	return (
-		<>
-			<footer className={classes.footer}>
-				<AppBar
-					position='fixed'
-					color='primary'
-					className={classes.appBar}
-				>
-					<Typography
-						variant='h6'
-						align='center'
-						gutterBottom
-					></Typography>
-				</AppBar>
-			</footer>
-		</>
-	);
+    const classes = useStyles();
+    return (
+        <>
+            <footer className={classes.footer}>
+                <AppBar
+                    position='fixed'
+                    color='primary'
+                    className={classes.appBar}
+                >
+                    <Typography
+                        variant='h6'
+                        align='center'
+                        gutterBottom
+                    ></Typography>
+                </AppBar>
+            </footer>
+        </>
+    );
 }
