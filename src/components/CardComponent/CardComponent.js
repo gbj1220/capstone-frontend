a8882b28c86d7397837171ae56af4d115bff5b28
Gregory.Johnson <3531op@gmail.com>
2021-09-12 08:41:54 -0400

    Fixed css for recipes page. Redux store is loosing track of jwtToken on refresh. Fix Immediately!

--- src/components/CardComponent/CardComponent.js
+++ src/components/CardComponent/CardComponent.js
@@ -3,133 +3,133 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-	Button,
-	Card,
-	CardActions,
-	CardContent,
-	CardMedia,
-	Typography,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
 } from '@material-ui/core';
 
 import './CardComponent.css';
 import { saveRecipeActionCreator } from '../../state-management/recipeState';
 
 const useStyles = makeStyles((theme) => ({
-	icon: {
-		marginRight: theme.spacing(2),
-	},
-	heroContent: {
-		backgroundColor: theme.palette.background.paper,
-		padding: theme.spacing(8, 0, 6),
-	},
-	heroButtons: {
-		marginTop: theme.spacing(4),
-	},
-	cardGrid: {
-		paddingTop: theme.spacing(8),
-		paddingBottom: theme.spacing(8),
-	},
-	card: {
-		margin: '25px',
-		boxShadow: '0 2px 20px black',
-		borderRadius: '10px',
-		width: '400px',
-		height: '400px',
-	},
-	cardMedia: {
-		paddingTop: '56%',
-	},
-	cardContent: {
-		flexGrow: 1,
-	},
-	cardButtons: {
-		justifyContent: 'center',
-		alignContent: 'center',
-		margin: '25px',
-	},
-	footer: {
-		backgroundColor: theme.palette.background.paper,
-		padding: theme.spacing(6),
-	},
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        margin: '25px',
+        boxShadow: '0 2px 20px black',
+        borderRadius: '10px',
+        width: '400px',
+        height: '400px',
+    },
+    cardMedia: {
+        paddingTop: '56%',
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    cardButtons: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        margin: '25px',
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
 }));
 
 export default function CardComponent(props) {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	const history = useHistory();
+    const history = useHistory();
 
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const jwtToken = useSelector((state) => state.login.jwtToken);
+    const jwtToken = useSelector((state) => state.login.jwtToken);
 
-	const label = props.hit.recipe.label;
-	const image = props.hit.recipe.image;
-	const recipeLink = props.hit.recipe.url;
-	const linkString = String(recipeLink);
+    const label = props.hit.recipe.label;
+    const image = props.hit.recipe.image;
+    const recipeLink = props.hit.recipe.url;
+    const linkString = String(recipeLink);
 
-	return (
-		<div key={props.index}>
-			<Card className={classes.card} style={{ margin: '20px' }}>
-				<CardMedia
-					className={classes.cardMedia}
-					image={image}
-					title='Image title'
-				/>
-				<CardContent className={classes.cardContent}>
-					<Typography gutterBottom variant='h5' component='h5'>
-						{label}
-					</Typography>
-				</CardContent>
-				<CardActions className={classes.cardButtons}>
-					{jwtToken ? (
-						<>
-							<Button
-								variant='outlined'
-								size='small'
-								color='primary'
-								onClick={() => {
-									window.open(linkString);
-								}}
-							>
-								Link to Recipe
-							</Button>
-							<Button
-								variant='outlined'
-								size='small'
-								color='primary'
-								onClick={() =>
-									dispatch(
-										saveRecipeActionCreator(
-											label,
-											image,
-											recipeLink
-										)
-									)
-								}
-							>
-								Save to Favorites
-							</Button>
-						</>
-					) : (
-						<>
-							<Button
-								size='small'
-								color='primary'
-								onClick={() => history.push('/error')}
-							>
-								View Recipe
-							</Button>
-							<Button
-								size='small'
-								color='primary'
-								onClick={() => history.push('/error2')}
-							>
-								Save to Favorites
-							</Button>
-						</>
-					)}
-				</CardActions>
-			</Card>
-		</div>
-	);
+    return (
+        <div key={props.index}>
+            <Card className={classes.card} style={{ margin: '20px' }}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={image}
+                    title='Image title'
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant='h5' component='h5'>
+                        {label}
+                    </Typography>
+                </CardContent>
+                <CardActions className={classes.cardButtons}>
+                    {jwtToken ? (
+                        <>
+                            <Button
+                                variant='outlined'
+                                size='small'
+                                color='primary'
+                                onClick={() => {
+                                    window.open(linkString);
+                                }}
+                            >
+                                Link to Recipe
+                            </Button>
+                            <Button
+                                variant='outlined'
+                                size='small'
+                                color='primary'
+                                onClick={() =>
+                                    dispatch(
+                                        saveRecipeActionCreator(
+                                            label,
+                                            image,
+                                            recipeLink
+                                        )
+                                    )
+                                }
+                            >
+                                Save to Favorites
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <Button
+                                size='small'
+                                color='primary'
+                                onClick={() => history.push('/error')}
+                            >
+                                View Recipe
+                            </Button>
+                            <Button
+                                size='small'
+                                color='primary'
+                                onClick={() => history.push('/error2')}
+                            >
+                                Save to Favorites
+                            </Button>
+                        </>
+                    )}
+                </CardActions>
+            </Card>
+        </div>
+    );
 }
--- src/components/FavoriteRecipes/FavoriteRecipes.js
+++ src/components/FavoriteRecipes/FavoriteRecipes.js
@@ -2,91 +2,92 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-	Box,
-	Card,
-	CardContent,
-	CardMedia,
-	Typography,
+    Box,
+    Button,
+    Card,
+    CardContent,
+    CardMedia,
+    Typography,
 } from '@material-ui/core';
 import { getRecipesActionCreator } from '../../state-management/favoriteRecipesState';
 
 const useStyles = makeStyles((theme) => ({
-	cardGrid: {
-		paddingTop: theme.spacing(8),
-		paddingBottom: theme.spacing(8),
-	},
-	card: {
-		margin: '25px',
-		boxShadow: '0 2px 20px black',
-		borderRadius: '10px',
-		width: '600px',
-		height: '400px',
-	},
-	cardMedia: {
-		paddingTop: '20%', // 16:9
-	},
-	cardContent: {
-		flexGrow: 1,
-	},
-	footer: {
-		backgroundColor: theme.palette.background.paper,
-		padding: theme.spacing(6),
-	},
-	mainCard: {
-		display: 'flex',
-		justifyContent: 'center',
-		objectFit: 'contain',
-	},
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        margin: '25px',
+        boxShadow: '0 2px 20px black',
+        borderRadius: '10px',
+        width: '600px',
+        height: '400px',
+    },
+    cardMedia: {
+        paddingTop: '50%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+    mainCard: {
+        display: 'flex',
+        justifyContent: 'center',
+        objectFit: 'contain',
+    },
 }));
 export default function FavoriteRecipes() {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	const recipesArr = useSelector(
-		(state) => state.favoriteRecipes.mainData.favoriteRecipes
-	);
-	console.log(`====== recipesArr ======`);
-	console.log(recipesArr);
-	const dispatch = useDispatch();
+    const recipesArr = useSelector(
+        (state) => state.favoriteRecipes.mainData.favoriteRecipes
+    );
+    console.log(`====== recipesArr ======`);
+    console.log(recipesArr);
+    const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getRecipesActionCreator());
-	}, []);
+    useEffect(() => {
+        dispatch(getRecipesActionCreator());
+    }, []);
 
-	return (
-		<div style={{ textAlign: 'center' }}>
-			<Box>
-				{recipesArr.map((recipe, i) => {
-					return (
-						<div key={i}>
-							<div className={classes.mainCard}>
-								<Card className={classes.card}>
-									<CardMedia
-										className={classes.cardMedia}
-										image={recipe.image}
-										title='Image title'
-									></CardMedia>
-									<CardContent
-										className={classes.cardContent}
-									>
-										<Typography
-											gutterBottom
-											variant='h5'
-											component='h5'
-										>
-											{recipe.label}
-										</Typography>
-									</CardContent>
-									<CardContent className={classes.cardMedia}>
-										<Typography>
-											{recipe.recipeLink}
-										</Typography>
-									</CardContent>
-								</Card>
-							</div>
-						</div>
-					);
-				})}
-			</Box>
-		</div>
-	);
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <Box
+                display='flex'
+                flexDirection='row'
+                flexWrap='wrap'
+                justifyContent='center'
+            >
+                {recipesArr.map((recipe, i) => {
+                    console.log(recipe.label);
+                    return (
+                        <Card
+                            className={classes.card}
+                            style={{ margin: '20px' }}
+                        >
+                            <CardMedia
+                                className={classes.cardMedia}
+                                image={recipe.image}
+                            />
+                            <CardContent>
+                                <Typography>{recipe.label}</Typography>
+                                <Button
+                                    variant='outlined'
+                                    size='small'
+                                    onClick={() =>
+                                        window.open(recipe.recipeLink)
+                                    }
+                                >
+                                    Go to Link
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    );
+                })}
+            </Box>
+        </div>
+    );
 }
